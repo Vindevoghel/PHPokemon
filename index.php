aa7dabf7144541fe5264df37f4e05034f23c8dfc
vindevoghel <yvindevoghel @gmail.com>
2019-10-02 16:25:59 +0200

    found previous evolution, moves

--- index.php
+++ index.php
@@ -13,26 +13,43 @@
 </form>
 <?php
 $pokeID = $_GET['pokeID'];
-$apiURL = 'https://pokeapi.co/api/v2';
+$apiURL = 'https://pokeapi.co/api/v2/pokemon/';
 
 function apiFetch($link) {
     $json = file_get_contents($link);
     return json_decode($json, true);
 }
 
-$pokeInfo = apiFetch($apiURL . '/pokemon/' . $pokeID);
+$pokeInfo = apiFetch($apiURL . $pokeID);
+$speciesInfo = apiFetch($pokeInfo[species][url]);
 //var_dump($pokeInfo);
+var_dump($speciesInfo[evolves_from_species][name]);
 
 $sprite = $pokeInfo[sprites][front_default];
+
 $name = $pokeInfo[name];
+
+$move1 = $pokeInfo[moves][0][move][name];
+$move2 = $pokeInfo[moves][1][move][name];
+$move3 = $pokeInfo[moves][2][move][name];
+$move4 = $pokeInfo[moves][3][move][name];
+
+$prevo = $speciesInfo[evolves_from_species][name];
+
 //echo $pokeInfo[species][url];
-echo $pokeInfo[moves][1][0];
-echo $pokeInfo[moves][1][1];
+//echo $pokeInfo[moves][0][move][name];
 
 ?>
 <h1>PHPokedex 1.0</h1>
-<h2><?php echo $name; ?></h2>
+<h2>Current Pokemon</h2>
+<h3><?php echo $name; ?></h3>
 <img src="<?php echo $sprite; ?>" alt="sprite">
+<p><?php echo $move1 ?></p>
+<p><?php echo $move2 ?></p>
+<p><?php echo $move3 ?></p>
+<p><?php echo $move4 ?></p>
+<h2>Previous Evolution</h2>
+<h3><?php echo $prevo; ?></h3>
 
-</body>.
+</body>
 </html>
